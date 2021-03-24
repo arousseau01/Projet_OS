7a4da43cf9b81166d386348feee5391901e856d7
sbrisson-sudo <80926815+sbrisson-sudo@users.noreply.github.com>
2021-03-24 17:53:38 +0100

    Ta version avec des mutex
    Passe le test

--- version_unix.ml
+++ version_unix.ml
@@ -1,5 +1,3 @@
-open Unix
-
 module type S = sig
   type 'a process
   type 'a in_port
@@ -13,23 +11,56 @@ module type S = sig
   val run: 'a process -> 'a
 end
 
-module Version_Unix (*:S*) = struct 
-  type 'a process = (unit -> 'a)
 
-  type 'a channel = 'a Queue.t
+module Mutex = struct
+  type t = Locked | Unlocked
+  let create () = let m = ref Unlocked in m
+  let lock (m: t ref) =
+    match !m with
+    | Unlocked -> ignore(m := Locked)
+    | Locked -> failwith "Mutex already locked"
+              
+  let unlock (m :t ref) =
+    match !m with
+    | Locked -> ignore(m := Unlocked)
+    | _ -> failwith "Mutex already unlocked"
+end
+             
+
+module Version_Unix :S = struct
+
+  open Queue
+  open Mutex
+  open Unix
+  
+  type 'a process = (unit -> 'a)
+  type 'a channel = { q: 'a Queue.t; m: Mutex.t ref; }
   type 'a in_port = 'a channel
   type 'a out_port = 'a channel
 
-  let new_channel () = Queue.create (), Queue.create ()
+  let new_channel () =
+    let channel = { q = Queue.create (); m = Mutex.create (); } in
+    channel, channel
   
-  let put v c () = Queue.push v c 
+  let put value channel () =
+    Mutex.lock channel.m;
+    Queue.push value channel.q;
+    Mutex.unlock channel.m
 
-  let get c () = Queue.pop c
+  let rec get channel () =
+      try
+        Mutex.lock channel.m;
+        let value = Queue.pop channel.q in
+        Mutex.unlock channel.m;
+        value 
+      with Queue.Empty ->
+        Mutex.unlock channel.m;
+        get channel () 
 
   let rec doco l () =
     match l with 
       | [] -> ()
-      | t::q -> begin match fork () with 
+      | t::q -> begin match Unix.fork () with 
         | 0 -> t ()
         | _ -> doco q ()
       end 
