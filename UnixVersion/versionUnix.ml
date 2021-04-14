c266d479d48161d4685ade6060bd3d2bf7fb8eb7
sbrisson <80926815+sbrisson-sudo@users.noreply.github.com>
2021-04-14 23:23:28 +0200

    Version à peu près fonctionelle
    Il y a encore un problème avec GET, je crois que le programme écrit trop dans la  pipe ce qui coupe la valeurs au bout de la FIFO

--- UnixVersion/versionUnix.ml
+++ UnixVersion/versionUnix.ml
@@ -28,7 +28,7 @@ end
 
 module Version_Unix :S = struct
 
-  (* open Marshal
+(* open Marshal
    * open Mutex
    * open Unix *)
   
@@ -39,7 +39,13 @@ module Version_Unix :S = struct
   type 'a in_port = 'a channel
   type 'a out_port = 'a channel
 
+  let return v  = (fun () -> v)
 
+  let run p = p ()
+
+  let bind p f =
+    (fun () -> f (run p) ())
+ 
   let new_channel () =
     let my_pipe = Unix.pipe () in
     let channel = { fd_out = snd my_pipe;
@@ -47,33 +53,42 @@ module Version_Unix :S = struct
                     m = Mutex.create (); } in
     channel, channel
   
-  let put value channel () =
-    Mutex.lock channel.m;
-    let out_chan = Unix.out_channel_of_descr channel.fd_out in
-    Marshal.to_channel out_chan value [];
-    Mutex.unlock channel.m
+  let put value channel = 
+    (fun () ->
+      Mutex.lock channel.m;
+      let out_chan = Unix.out_channel_of_descr channel.fd_out in
+      Marshal.to_channel out_chan value [Marshal.Closures];
+      flush out_chan;
+      Mutex.unlock channel.m
+    )
+  let get channel =
+    (fun () ->
+      let in_chan = Unix.in_channel_of_descr channel.fd_in in
+      let rec next chan mutex = 
+        try
+          Mutex.lock mutex;
+          let value = Marshal.from_channel chan in
+          Mutex.unlock mutex; value
+        with End_of_file ->
+          Mutex.unlock mutex;
+          next chan mutex
+          in
+        next in_chan channel.m
+    )
 
-  let rec get channel () =
-    let in_chan = Unix.in_channel_of_descr channel.fd_in in
-      try
-        Mutex.lock channel.m;
-        let value = Marshal.from_channel in_chan in
-        Mutex.unlock channel.m; value
-      with End_of_file ->
-        Mutex.unlock channel.m;
-        get channel () 
-
-  let rec doco l () =
-    match l with 
-      | [] -> ()
-      | t::q -> begin match Unix.fork () with 
-        | 0 -> t ()
-        | _ -> doco q ()
-      end 
+  let rec doco l =
+    (fun () ->
+      let rec next l =
+        match l with 
+          | [] -> ()
+          | [t] -> run t
+          | t::q ->  begin 
+            match Unix.fork () with 
+              | 0 -> run t ; exit 0
+              | _ -> next q 
+            end
+      in
+      next l
+    )
   
-  let return v () = v
-
-  let bind p f () = f (p ()) ()
-
-  let run p = p ()
 end
