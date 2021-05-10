a0aee6bba64006f6d5cd8b180015f7b553452ff7
Arthur Rousseau <arthur.rousseau@orange.fr>
2021-05-10 15:16:04 +0200

    Strassen

--- SequentialVersion/version_sequentielle.ml
+++ SequentialVersion/version_sequentielle.ml
@@ -39,7 +39,7 @@ module Monad = struct (* Inutile mais a servi à comprendre l'article de Koen *)
 
 end
 
-module VS : S  =
+module VS : S  = (* Version Séquentielle *)
   struct
     let actions : (unit -> unit) Queue.t = Queue.create () (* Ensemble des actions à effectuer sous forme de queue *)
 
@@ -74,14 +74,16 @@ module VS : S  =
       suivant ()
 
     let doco (l : unit process list) (c : unit -> unit) =
-        List.iter (fun p -> add (fun () -> p suivant)) l ;
-        ignore (suivant ()) ;
-        c ()
+      (* On ajoute toutes les actions à faire à la queue, on termine la queue puis on fait la continuation *)
+      List.iter (fun p -> add (fun () -> p suivant)) l ;
+      ignore (suivant ()) ;
+      c ()
 
     let return (a:'a) (c:'a -> unit) = c a
     let bind (p1 : 'a process) (fp2 : 'a -> 'b process) (c : 'b -> unit) = p1 (fun a -> (fp2 a) c)
 
     let run p = 
+      (* Comme on a pas accès directement aux valeurs renvoyées par un processus, on se sert de la continuation pour modifier une référence *)
       let result = ref(None) in 
       let c = (fun a -> result:= Some a; suivant ()) in p c;
       match !result with 
@@ -118,9 +120,6 @@ module Example (K : S) = struct
       (fun (q_in, q_out) -> K.doco [ integers q_out ; output q_in ; ])
 end
 
-
-
-
 module Crible (K : S) = struct 
 
   module Lib = Lib(K)
@@ -134,7 +133,7 @@ module Crible (K : S) = struct
   
   let output (qi : int K.in_port) : unit K.process =
     let rec loop () =
-      (K.get qi) >>= (fun v -> Printf.printf "%d\n" v; loop ())
+      (K.get qi) >>= (fun v -> Format.printf "%d@." v; loop ())
     in loop ()
 
   let filter (prime:int) (qi: int K.in_port) (qo:int K.out_port) : unit K.process = 
@@ -153,12 +152,137 @@ module Crible (K : S) = struct
         )
 
   let main : unit K.process = 
-    Printf.printf "Je commence";
     (delay (fun () -> K.new_channel (),K.new_channel ()) () ) >>=
       (fun ((q1_in, q1_out), (q2_in, q2_out)) -> K.doco [ integers q1_out; sift q1_in q2_out; output q2_in ])
 
 end 
 
-module Test = Crible(VS);;
+module Strassen (K : S) = struct
 
-VS.run Test.main;;
+  module Lib = Lib(K)
+  open Lib
+
+  let scalar (n:int) (a:int array array) = 
+    let a' = Array.make_matrix (Array.length a) (Array.length a.(0)) 0 in 
+    for i = 0 to Array.length a - 1 do
+      for j=0 to Array.length a.(0) -1 do
+        a'.(i).(j) <- a.(i).(j)*n
+      done;
+    done;
+    a'
+  
+  let add (a:int array array) (b:int array array) = 
+    if Array.length a <> Array.length b || Array.length a.(0) <> Array.length b.(0) 
+      then failwith "Pas la bonne taille (somme)"
+    else let c = Array.make_matrix (Array.length a) (Array.length a.(0)) 0 in 
+    for i = 0 to Array.length a - 1 do
+      for j=0 to Array.length a.(0) -1 do
+        c.(i).(j) <- a.(i).(j) + b.(i).(j)
+      done;
+    done;
+    c
+
+  let sub (a:int array array) (b:int array array) = add a (scalar (-1) b)
+
+  let cut (a:int array array) =
+    let na,ma = Array.length a,Array.length a.(0) in 
+    let na',ma' = na/2,ma/2 in 
+    let a1,a2,a3,a4 = Array.make_matrix na' ma' 0, Array.make_matrix na' ma' 0, Array.make_matrix na' ma' 0, Array.make_matrix na' ma' 0 in 
+    for i=0 to na'-1 do 
+      for j=0 to ma'-1 do 
+        a1.(i).(j) <- a.(i).(j)
+      done;
+      for j= ma' to ma -1 do 
+        a2.(i).(j-ma') <- a.(i).(j)
+      done;
+    done;
+    for i=na' to na-1 do 
+      for j=0 to ma'-1 do 
+        a3.(i-na').(j) <- a.(i).(j)
+      done;
+      for j= ma' to ma -1 do 
+        a4.(i-na').(j-ma') <- a.(i).(j)
+      done;
+    done;
+    a1,a2,a3,a4
+
+  let concat c11 c12 c21 c22 = 
+    let n = Array.length c11 in 
+    let c = Array.make_matrix (2*n) (2*n) 0 in 
+    for i = 0 to n-1 do 
+      for j = 0 to n-1 do 
+        c.(i).(j) <- c11.(i).(j)
+      done;
+      for j = n to 2*n-1 do 
+        c.(i).(j) <- c12.(i).(j-n)
+      done;
+    done;
+    for i = n to 2*n-1 do 
+      for j = 0 to n-1 do 
+        c.(i).(j) <- c21.(i-n).(j)
+      done;
+      for j = n to 2*n-1 do 
+        c.(i).(j) <- c22.(i-n).(j-n)
+      done;
+    done;
+    K.return c 
+
+  let impression (a:int array array) = 
+    K.return (
+    let n,m = Array.length a, Array.length a.(0) in 
+    for i=0 to n-1 do 
+      for j=0 to n-1 do 
+        Format.printf "%d " a.(i).(j)
+      done;
+      Format.printf "\n"
+    done;
+    )
+
+  let rec mat_mult_p2 (a:int array array) (b:int array array) =
+    let na,ma,nb,mb = Array.length a,Array.length a.(0),Array.length b, Array.length b.(0) in 
+    if na<>ma || na<>nb || na<>mb 
+      then failwith "Pas la bonne taille (produit)"
+    else if na = 1
+      then K.return [|[|a.(0).(0)*b.(0).(0)|]|]
+    else 
+      let a11,a12,a21,a22 = cut a in 
+      let b11,b12,b21,b22 = cut b in 
+      let m1,m2,m3,m4,m5,m6,m7 = ref([|[||]|]),ref([|[||]|]),ref([|[||]|]),ref([|[||]|]),ref([|[||]|]),ref([|[||]|]),ref([|[||]|]) in
+      let p1 = K.return (m1:=K.run (mat_mult_p2 (add a11 a22) (add b11 b22))) in 
+      let p2 = K.return (m2:=K.run (mat_mult_p2 (add a21 a22) b11)) in 
+      let p3 = K.return (m3:=K.run (mat_mult_p2 a11 (sub b12 b22))) in 
+      let p4 = K.return (m4:=K.run (mat_mult_p2 a22 (sub b21 b11))) in 
+      let p5 = K.return (m5:=K.run (mat_mult_p2 (add a11 a12) b22)) in 
+      let p6 = K.return (m6:=K.run (mat_mult_p2 (sub a21 a11) (add b11 b12))) in 
+      let p7 = K.return (m7:=K.run (mat_mult_p2 (sub a12 a22) (add b21 b22))) in 
+      (K.doco [p1;p2;p3;p4;p5;p6;p7]) >>= (fun () -> concat (add (add !m1 !m4) (sub !m7 !m5)) (add !m3 !m5) (add !m2 !m4) (add (sub !m1 !m2) (add !m3 !m6)) )
+
+end
+
+let a1=[|[|1|]|]
+let a2=[|
+  [|1;0|];
+  [|0;1|]
+|]
+let a4=[| 
+  [|1;2;3;4|];
+  [|5;6;7;8|];
+  [|9;10;11;12|];
+  [|13;14;15;16|]
+|]
+
+let b1=[|[|1|]|]
+let b2=[| 
+  [|1;0|];
+  [|0;1|]
+|]
+let b4=[| 
+  [|1;0;0;0|];
+  [|0;1;0;0|];
+  [|0;0;1;0|];
+  [|0;0;0;1|]
+|]
+
+module Test = Strassen(VS);;
+
+VS.run (Test.impression (VS.run (Test.mat_mult_p2 a4 b4)));;
